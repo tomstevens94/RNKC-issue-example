cc554f410d26d40a720ab20da88cadeb10fff812
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2025-02-28 12:11:01 +0000

    ♻️ Remove keyboard components

--- App.tsx
+++ App.tsx
@@ -1,53 +1,53 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
-import {Text, TextInput, TouchableOpacity} from 'react-native';
 import {
-  KeyboardAvoidingView,
-  KeyboardProvider,
-} from 'react-native-keyboard-controller';
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import React from 'react';
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 
-const Screen1 = props => {
+const Screen1 = (props: NativeStackScreenProps<any>) => {
+  const progress = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    paddingBottom: interpolate(progress.value, [0, 1], [0, 200]),
+  }));
+
   return (
-    <KeyboardAvoidingView
-      behavior="padding"
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        backgroundColor: 'red',
-      }}>
+    <Animated.View style={[styleSheet.screen1, animatedStyle]}>
       <Text>Screen 1</Text>
-      <TouchableOpacity style={{backgroundColor: 'yellow', padding: 48}}>
-        <Text>TEST BUTTON</Text>
+      <TouchableOpacity
+        style={styleSheet.animateButton}
+        onPress={() =>
+          (progress.value = withTiming(progress.value === 0 ? 1 : 0))
+        }>
+        <Text>ANIMATE</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => props.navigation.navigate('screen2')}
-        style={{backgroundColor: 'green', padding: 48}}>
+        style={styleSheet.button1}>
         <Text>Go to screen 2</Text>
       </TouchableOpacity>
-    </KeyboardAvoidingView>
+    </Animated.View>
   );
 };
 
-const Screen2 = props => {
+const Screen2 = (props: NativeStackScreenProps<any>) => {
   return (
-    <KeyboardAvoidingView
-      behavior="padding"
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        backgroundColor: 'green',
-      }}>
+    <Animated.View style={styleSheet.screen2}>
       <Text>Screen 2</Text>
-      <TextInput style={{backgroundColor: 'white', padding: 32, width: 100}} />
       <TouchableOpacity
         onPress={() => props.navigation.navigate('screen1')}
-        style={{backgroundColor: 'blue', padding: 48}}>
+        style={styleSheet.button2}>
         <Text>Go to screen 1</Text>
       </TouchableOpacity>
-    </KeyboardAvoidingView>
+    </Animated.View>
   );
 };
 
@@ -55,15 +55,34 @@ const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <KeyboardProvider>
+    <>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="screen1" component={Screen1} />
           <Stack.Screen name="screen2" component={Screen2} />
         </Stack.Navigator>
       </NavigationContainer>
-    </KeyboardProvider>
+    </>
   );
 }
 
 export default App;
+
+const styleSheet = StyleSheet.create({
+  container: {flex: 1},
+  screen1: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'red',
+  },
+  screen2: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'green',
+  },
+  animateButton: {backgroundColor: 'white', padding: 16},
+  button1: {backgroundColor: 'yellow', padding: 48},
+  button2: {backgroundColor: 'blue', padding: 48},
+});
